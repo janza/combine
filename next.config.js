26df5c3d520397acb54cc0ae5c610241fd7fca4d
Josip Janzic <josip@jjanzic.com>
2017-11-04 14:10:22 +0100

    Use react for production too

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 module.exports = {
+  exportPathMap () {
+    return {
+      "/": { page: "/" },
+    }
+  },
   webpack: function (config, { dev }) {
     // For the development version, we'll use React.
     // Because, it supports react hot loading and so on.
-    if (dev) {
+    // if (dev) {
       return config
-    }
+    // }
 
     config.resolve.alias = {
-      'react': 'preact-compat/dist/preact-compat',
-      'react-dom': 'preact-compat/dist/preact-compat'
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
     }
 
     return config
