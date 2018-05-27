2b40203946dee2625d4fcc2d51a35a839453a1c1
Josip Janzic <josip@jjanzic.com>
2018-05-27 18:00:51 +0200

    Move killed blocks out of state object

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default class Page extends React.Component {
           }}
         >
           <GameBoard
-            width={500}
+            width={600}
             colors={this.game.colors()}
             score={this.game.score()}
             highScore={this.game.highScore()}
